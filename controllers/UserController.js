ead9c535c40fd974dc975eb0df090e2ebe263e53
siobahnschuck <sschuc@saic.edu>
2021-02-25 16:08:58 -0600

    nodemon in json + userController

---
+++ controllers/UserController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User')
+
+const createUser = async (req, res) => {
+    try{
+        const user = await new User(req.body)
+        await user.save()
+        return res.status(201).json({ user })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+module.exports = {
+    createUser,
+}
