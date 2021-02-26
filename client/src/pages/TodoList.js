92d9d7193335b72723b668eb3bde8a1213ab8a56
queeniewilliams <queeniepu329@gmail.com>
2021-02-26 07:06:03 -0800

    fixed missing syntax

--- client/src/pages/TodoList.js
+++ client/src/pages/TodoList.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 export default class TodoList extends Component {
-constructor() {
+  constructor() {
     super()
     this.state = {
-    listItem: '',
-    todoList: [],
-    deadline: ''
+      listItem: '',
+      todoList: [],
+      deadline: ''
     }
+  }
   getAllTodos = async () => {
-
     try {
-    const response = await axios.get('http://localhost:3001/api/todos')
-    console.log(response)
+      const response = await axios.get('http://localhost:3001/api/todos')
+      console.log(response)
     } catch (error) {
-    console.log('error')
+      console.log('error')
     }
+  }
 
-addItem = (e) => {
+  addItem = (e) => {
     e.preventDefault()
     const newArray = this.state.todoListArray
     newArray.push(this.state.newItem)
-    this.setState({newItem: '', todoListArray: newArray })
-}
-clearlist(e){
+    this.setState({ newItem: '', todoListArray: newArray })
+  }
+  clearlist(e) {
     this.setState({
-        newArray:[]
+      newArray: []
     })
-}
+  }
 
   handleChange = (event) => {
     this.setState({ listItem: event.target.value })
@@ -35,44 +36,45 @@ clearlist(e){
   handleSubmit = async (e) => {
     e.preventDefault()
     try {
-    console.log('submitted firing off')
+      console.log('submitted firing off')
       //axios call
-    let response = await axios.post('http://localhost:3001/api/todos/add', {
+      let response = await axios.post('http://localhost:3001/api/todos/add', {
         listItem: this.state.listItem
-    })
-    console.log(response.data)
-    return response.data
+      })
+      console.log(response.data)
+      return response.data
     } catch (error) {
-    console.log(error)
+      console.log(error)
     }
-render() {
-    let todoItems= this.props.thelist.map((item,index) =>(
-        <listItem doThis={item} key={'todo'+ index}/>
+  }
+  render() {
+    let todoItems = this.props.thelist.map((item, index) => (
+      <listItem doThis={item} key={'todo' + index} />
     ))
     console.log(this.state.listItem)
     return (
-    <div>
+      <div>
         <h1>todolist</h1>
         <form onSubmit={this.handleSubmit}>
-        <input
+          <input
             type="text"
             name="listItem"
             placeholder="add todo here"
             value={this.state.listItem}
             onChange={this.handleChange}
-        />
-        <input
+          />
+          <input
             type="text"
             name="deadline"
             placeholder="deadline"
             value={this.state.deadline}
             onChange={this.handleDeadline}
-        />
-        <button onClick={(e)=> this.additem(e)} >Add item!</button>
-        <input type="submit" value="submit" />
+          />
+          <button onClick={(e) => this.additem(e)}>Add item!</button>
+          <input type="submit" value="submit" />
         </form>
-        <button onClick={(e) => this.clearlist(e)} >Clear list!</button>
-    </div>
+        <button onClick={(e) => this.clearlist(e)}>Clear list!</button>
+      </div>
     )
-}
+  }
 }
